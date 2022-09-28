2abc6623086b41c5179ca5c1a4b5b3a83400abb8
zhuhe36784 <zhuhe36784@hundsun.com>
2022-09-28 17:57:00 +0800

    11

--- packages/main.ts
+++ packages/main.ts
@@ -1,3 +1,3 @@
-export { default as hChart } from './chart'
+export { default as HChart } from './chart'
 export { default as HHistogram } from './histogram'
 export { default as HPie } from './pie'
